2a33c7011b35e2176059978b294a2ec6ceb8b65f
Michael Ballard <ballard.michael9@gmail.com>
2018-06-11 10:52:51 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,13 +4,14 @@ class PostsController < ApplicationController
   end
   
   def new
-   
     
   end
   
-  def create 
 
+    def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+    end
 
-end 
   
 end
